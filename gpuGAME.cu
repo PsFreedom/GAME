519fd75abc68695651505b9e3a9005a7895a1bec
PsFreedom <Ps_Freedom@Hotmail.com>
2021-01-11 23:27:09 -0600

    Baseline GAME
    - Prototype
    - The first working version of GPU-Assisted Memory Expansion.

--- gpuGAME.cu
+++ gpuGAME.cu
@@ -5,63 +5,73 @@
 
 #define THREAD_MODEL NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS
 
-int64_t gpuGAME_SIZE;
-int64_t *gpuGAME_PTR;
+uint64_t gpuGAME_SIZE;
+uint64_t *gpuGAME_PTR;
 
 static void 
-gpuGAME_load (void)
-{
-    printf("gpuGAME: gpuGAME is loaded\n");
+gpuGAME_load (void){
+    printf("\tgpuGAME >> gpuGAME is loaded\n");
 }
 
 static void *
-gpuGAME_open (int readonly)
-{
+gpuGAME_open (int readonly){
     /* create a handle ... */
-    printf("gpuGAME: gpuGAME is opened\n");
+    printf("\tgpuGAME >> gpuGAME is opened\n");
     return NBDKIT_HANDLE_NOT_NEEDED;
+}
+
+static int64_t 
+gpuGAME_get_size (void *handle){
+    return gpuGAME_SIZE;
 }
 
 static int 
 gpuGAME_config(const char *key, const char *value)
 {
+	cudaError_t cuda_errno;
     if( strcmp(key, "size")==0 ){
         gpuGAME_SIZE = nbdkit_parse_size(value);
-        printf("gpuGAME: gpuGAME_SIZE (%s) = %ld\n", value, gpuGAME_SIZE);
+        printf("\tgpuGAME >> SIZE %s = %ld\n", value, gpuGAME_SIZE);
+		printf("\tgpuGAME >> GPU memory pre-alloc - %p\n", gpuGAME_PTR);
 		
-		if( cudaMalloc(&gpuGAME_PTR, gpuGAME_SIZE) == cudaSuccess ){
-			printf("gpuGAME: GPU memory allocated!\n");
+		cuda_errno = cudaMalloc(&gpuGAME_PTR, gpuGAME_SIZE);
+		if( cuda_errno == cudaSuccess ){
+			printf("\tgpuGAME >> GPU memory allocated - %p\n", gpuGAME_PTR);
 		}
 		else{
-			printf("gpuGAME: GPU memory allocation failed!\n");
+			printf("\tgpuGAME >> GPU memory allocation failed! (cuda_errno %u)\n", cuda_errno);
 			return -1;
 		}
     }
     else{
-        printf("gpuGAME: do not recognize this config\n");
+        printf("\tgpuGAME >> do not recognize this config\n");
 		return -1;
     }
 	return 0;
 }
 
-static int64_t 
-gpuGAME_get_size (void *handle)
-{
-    return gpuGAME_SIZE;
-}
-
 static int 
 gpuGAME_pread (void *handle, void *buf, uint32_t count, uint64_t offset, uint32_t flags)
 {
-    cudaMemcpy( buf, gpuGAME_PTR + offset, count, cudaMemcpyDeviceToHost );
-    return 0;
+	cudaError_t cuda_errno;
+	cuda_errno = cudaMemcpy(buf, (gpuGAME_PTR + offset/sizeof(uint64_t)), count, cudaMemcpyDeviceToHost);
+	if(cuda_errno != cudaSuccess){
+		printf("\tgpuGAME >> cudaMemcpy ERROR %d\n", cuda_errno);
+		return -1;
+	}
+	return 0;
 }
 
 static int 
 gpuGAME_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset, uint32_t flags)
 {
-    cudaMemcpy( gpuGAME_PTR + offset, buf, count, cudaMemcpyHostToDevice );
-    return 0;
+	cudaError_t cuda_errno;
+    cuda_errno = cudaMemcpy((gpuGAME_PTR + offset/sizeof(uint64_t)), buf, count, cudaMemcpyHostToDevice);
+	if(cuda_errno != cudaSuccess){
+		printf("\tgpuGAME >> cudaMemcpy ERROR %d\n", cuda_errno);
+		return -1;
+	}
+	return 0;
 }
 
 static struct nbdkit_plugin plugin = {
