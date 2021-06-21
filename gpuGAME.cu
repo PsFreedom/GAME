a834bd6ad172c5ed194cb0d1b8db57b9daf9211e
PsFreedom <Ps_Freedom@Hotmail.com>
2021-06-21 01:44:43 -0500

    Base Multi GPU
    Baseline for Multi GPUs

--- gpuGAME.cu
+++ gpuGAME.cu
@@ -6,30 +6,100 @@
 #include <assert.h>
 
 #define THREAD_MODEL NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS
+#define BLOCK_SZ 4096UL               // 4KB
+#define SOFT_LIM (1024UL*1024*1024*3) // 4GB
+#define HARD_LIM (1024UL*1024*1024)   // 1GB
+#define AREA_SZ  (BLOCK_PER_AREA*BLOCK_SZ)
 
+int err_no;
 uint64_t GAME_SIZE;
-char    *GAME_PTR;
+uint64_t BLOCK_PER_AREA;
 cudaStream_t read_stream;
 cudaStream_t write_stream;
 
-int err_no;
+struct area_desc{
+    uint8_t  GPU_ID;
+    uint64_t start_offset;
+    uint64_t last_offset;
+    uint64_t *vram_ptr;
+    bool *PBV;
+    struct area_desc *next;
+};
+
 int num_gpus = -1;
+struct area_desc *area_root = NULL;
+
+uint64_t MIN( uint64_t A, uint64_t B ){
+    if( A <= B )
+        return A;
+    return B;
+}
+
+struct area_desc* find_area( uint64_t offset )
+{
+    struct area_desc *curr = area_root;
+    while( curr != NULL ){
+        if( offset >= curr->start_offset && offset < curr->last_offset )
+            return curr;
+        curr = curr->next;
+    }
+    return NULL;
+}
+
+struct area_desc* make_area( uint64_t offset )
+{
+    uint64_t *vram_ptr = NULL;
+    struct area_desc *curr;
+    
+    for( int gpu_id = 0; gpu_id < num_gpus; gpu_id++ ) 
+    {
+        size_t free_mem = 0, tot_mem = 0;
+        
+        err_no = cudaSetDevice( gpu_id );
+        assert( err_no == cudaSuccess);
+        
+        cudaMemGetInfo( &free_mem, &tot_mem );
+        if( free_mem > SOFT_LIM )
+        {
+            printf("\tGAME >> %s: make an area for %p\n", __FUNCTION__, (void*)offset );
+            printf("\tGAME >> %s: Device %d - Free %zd Total %zd\n", __FUNCTION__, gpu_id, free_mem, tot_mem );
+            err_no = cudaMalloc( &vram_ptr, AREA_SZ );
+            assert( err_no == cudaSuccess );
+            assert( vram_ptr != NULL );
+            
+            curr = (struct area_desc*)malloc(sizeof(struct area_desc));
+            curr->GPU_ID       = gpu_id;
+            curr->start_offset = (offset/AREA_SZ)   * AREA_SZ;
+            curr->last_offset  = curr->start_offset + AREA_SZ;
+            curr->vram_ptr     = vram_ptr;
+            curr->PBV          = NULL;
+            curr->next         = area_root;
+            area_root = curr;
+            
+            printf("\tGAME >> %s: GPU %d\n", __FUNCTION__, curr->GPU_ID );
+            printf("\tGAME >> %s: offset %p - %p\n", __FUNCTION__, (void*)curr->start_offset, (void*)curr->last_offset );
+            printf("\tGAME >> %s: ptr %p\n", __FUNCTION__, curr->vram_ptr );
+            return curr;
+        }
+    }
+    return NULL;
+}
 
 static void *
 gpuGAME_open (int readonly){
     /* create a handle ... */
     
-    err_no = cudaGetDeviceCount( &num_gpus );
-    assert( err_no == cudaSuccess);
-    printf("\n\tGAME >> %s: Available CUDA capable devices: %d\n", __FUNCTION__, num_gpus );
+    cudaGetDeviceCount( &num_gpus );
+    printf("\tGAME >> %s: Blocks per Area %lu, Size %lu\n", __FUNCTION__, BLOCK_PER_AREA, GAME_SIZE );
+    printf("\tGAME >> %s: CUDA devices: %d\n", __FUNCTION__, num_gpus );
     
     for( int gpu_id = 0; gpu_id < num_gpus; gpu_id++ ) 
     {
-        size_t free_mem, tot_mem;
+        size_t free_mem = 0, tot_mem = 0;
         
         err_no = cudaSetDevice( gpu_id );
         assert( err_no == cudaSuccess);
-        
+
         cudaMemGetInfo( &free_mem, &tot_mem );        
         printf("\tGAME >> %s: Device %d - Free %zd Total %zd\n", __FUNCTION__, gpu_id, free_mem, tot_mem );
     }
@@ -47,15 +117,16 @@ gpuGAME_config(const char *key, const char *value)
 {
     if( strcmp(key, "size")==0 )
     {
-        GAME_SIZE = nbdkit_parse_size( value );
-        err_no    = cudaMalloc( &GAME_PTR, GAME_SIZE );
-        assert( err_no == cudaSuccess );
-        
+        GAME_SIZE = nbdkit_parse_size( value );        
         cudaStreamCreate( &read_stream );
         cudaStreamCreate( &write_stream );
     }
+    else if( strcmp(key, "blk_per_area")==0 )
+    {
+        nbdkit_parse_uint64_t( "blk_per_area", value, &BLOCK_PER_AREA );
+    }
     else{
-        printf("%s: Unknow argument\n", __FUNCTION__ );
+        printf("\tGAME >> %s: Unknow argument\n", __FUNCTION__ );
         return -1;
     }
     return 0;
@@ -64,18 +135,68 @@ gpuGAME_config(const char *key, const char *value)
 static int 
 gpuGAME_pread (void *handle, void *buf, uint32_t count, uint64_t offset, uint32_t flags)
 {
-    err_no = cudaMemcpyAsync( buf, (GAME_PTR + offset), count, cudaMemcpyDeviceToHost, read_stream );
-//  printf("%s: err_no %d\n", __FUNCTION__, err_no );
-    assert( err_no == cudaSuccess);
+    struct area_desc *curr;
+    uint64_t *vram_addr;
+    uint64_t curr_offset = offset;
+    uint64_t end_offset;
+    uint32_t left_count = count;
+    uint32_t curr_count;
+    
+    while( curr_offset < offset + count )
+    {
+        curr = find_area( curr_offset );
+        if( curr == NULL )
+            curr = make_area( curr_offset );
+            
+        err_no = cudaSetDevice( curr->GPU_ID );
+        assert( err_no == cudaSuccess);
+        
+        end_offset = MIN( curr_offset + left_count, curr->last_offset );
+        curr_count = end_offset - curr_offset;
+        vram_addr  = (uint64_t*)((uint64_t)curr->vram_ptr + (uint64_t)(curr_offset % AREA_SZ));
+        
+        //printf("\tGAME >> %s: ADDR %p count %u\n", __FUNCTION__, vram_addr, curr_count );
+        err_no = cudaMemcpyAsync( buf, vram_addr, curr_count, cudaMemcpyDeviceToHost, read_stream );
+        assert( err_no == cudaSuccess);
+        
+        curr_offset = end_offset;
+        left_count -= curr_count;
+    }
+    assert( left_count == 0 );
     return 0;
 }
 
 static int 
 gpuGAME_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset, uint32_t flags)
 {
-    err_no = cudaMemcpyAsync( (GAME_PTR + offset), buf, count, cudaMemcpyHostToDevice, write_stream );
-//  printf("%s: err_no %d\n", __FUNCTION__, err_no );
-    assert( err_no == cudaSuccess);
+    struct area_desc *curr;
+    uint64_t *vram_addr;
+    uint64_t curr_offset = offset;
+    uint64_t end_offset;
+    uint32_t left_count = count;
+    uint32_t curr_count;
+    
+    while( curr_offset < offset + count )
+    {
+        curr = find_area( curr_offset );
+        if( curr == NULL )
+            curr = make_area( curr_offset );
+            
+        err_no = cudaSetDevice( curr->GPU_ID );
+        assert( err_no == cudaSuccess);
+        
+        end_offset = MIN( curr_offset + left_count, curr->last_offset );
+        curr_count = end_offset - curr_offset;
+        vram_addr  = (uint64_t*)((uint64_t)curr->vram_ptr + (uint64_t)(curr_offset % AREA_SZ));
+        
+        //printf("\tGAME >> %s: ADDR %p count %u\n", __FUNCTION__, vram_addr, curr_count );
+        err_no = cudaMemcpyAsync( vram_addr, buf, curr_count, cudaMemcpyHostToDevice, write_stream );
+        assert( err_no == cudaSuccess);
+        
+        curr_offset = end_offset;
+        left_count -= curr_count;
+    }
+    assert( left_count == 0 );
     return 0;
 }
 
